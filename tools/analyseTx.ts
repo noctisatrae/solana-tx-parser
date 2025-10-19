414022e5f05b5354aedca4244d3ce006d261e88f
Hector <noctisatrae@pm.me>
2025-10-19 15:16:26 -0400

    feat: improve tool

--- tools/analyseTx.ts
+++ tools/analyseTx.ts
@@ -1,9 +1,13 @@
-import { createSolanaRpc, type Signature } from "@solana/kit";
+import { createSolanaRpc, signature, type Signature } from "@solana/kit";
 import { analyzeTransaction } from "../src";
 
-const rpc = createSolanaRpc("https://api.mainnet-beta.solana.com");
+const rpc = createSolanaRpc(
+  process.env["TESTRUNNER_RPC"] || "https://api.mainnet-beta.solana.com",
+);
 
+const [, , sigArg] = Bun.argv;
 const swapTxSig =
-  "2xRQGcC2xJFBQ4c5wa71uDpj4GwjpiDkGX83GQxL9P97iwLiJkKymR3CpyP6as8tjMK2hZ2iuDg997nrRLXD4UeY" as Signature;
+  (sigArg ? signature(sigArg) : undefined) ||
+  ("2xRQGcC2xJFBQ4c5wa71uDpj4GwjpiDkGX83GQxL9P97iwLiJkKymR3CpyP6as8tjMK2hZ2iuDg997nrRLXD4UeY" as Signature);
 
 console.debug(await analyzeTransaction(rpc, swapTxSig));
